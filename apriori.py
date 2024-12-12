a3b4a7fb61346e56d6324e20b9030f018ac700b8
m9tya <144878865+m9tya@users.noreply.github.com>
2024-12-12 10:56:29 +0300

    adding algorithm files

--- apriori.py
+++ apriori.py
@@ -7,49 +7,6 @@ from apyori import apriori
 import seaborn as sns
 import matplotlib.pyplot as plt
 import networkx as nx
-
-# class Apriori:
-#     @staticmethod
-#     def apriori_algorithm():
-#
-#         df = pd.read_csv("D:\\university\\7_semestr\\kursach\\dataset\\filtered_dataset.csv")
-#
-#         transactions = []
-#         for _, row in df.iterrows():
-#             transaction = [str(item) for item in row if pd.notna(item)]
-#             transactions.append(transaction)
-#
-#         results = list(apriori(transactions, min_support=0.2, min_confidence=0.3, min_lift=1))
-#
-#         output_file_path = "D:\\university\\7_semestr\\kursach\\dataset\\apriori_results.txt"
-#
-#         with open(output_file_path, mode='w', encoding='utf-8') as file:
-#             arr = []
-#
-#             for relation_record in results:
-#                 itemset = relation_record.items
-#                 support = relation_record.support
-#                 if len(itemset) == 1:
-#                     if '' not in itemset:
-#                         result = {"itemset": itemset, "support": support}
-#                         arr.append(result)
-#                         file.write(f"Itemset: {itemset} (Support: {support:.2f})\n")
-#                 else:
-#                     ordered_statistics = relation_record.ordered_statistics
-#                     for rule in ordered_statistics:
-#                         antecedent = rule.items_base
-#                         consequent = rule.items_add
-#                         confidence = rule.confidence
-#                         lift = rule.lift
-#                         if '' not in antecedent and '' not in consequent:
-#                             file.write(f"Правило: {antecedent} -> {consequent}\n")
-#                             file.write(f"  Поддержка: {support:.2f}\n")
-#                             file.write(f"  Уверенность: {confidence:.2f}\n")
-#                             file.write(f"  Лифт: {lift:.2f}\n")
-#                             file.write("\n")
-#
-#                             arr.append({"antecedent": antecedent, "consequent": consequent,
-#                                         "support": support, "confidence": confidence, "lift": lift})
 import time
 import psutil
 
